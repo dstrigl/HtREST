7137f913222fd5927e71920b7468e670167c3bcf
Daniel Strigl <strigl.daniel@gmail.com>
2020-01-03 19:00:45 +0100

    ongoing work

--- tests/test_version.py
+++ tests/test_version.py
@@ -17,7 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-""" Tests for code in htheatpump.version. """
+""" Tests for code in htrest.version. """
 
 from htrest.version import Version
 
