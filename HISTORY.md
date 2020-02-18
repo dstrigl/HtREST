3fd93f3aa5b523e5dd4bf92f74f59d12c10d34a7
dast <my@mail.com>
2020-02-18 20:42:43 +0100

    Fixed wrong version definition.

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # History
 
+## 0.1.2 (2020-02-18)
+
+* Fixed wrong version definition.
+
 ## 0.1.1 (2020-02-18)
 
 * Fixed some errors in the project packaging.
--- htrest/version.py
+++ htrest/version.py
@@ -68,6 +68,6 @@ class Version:
         return "[{}, version {}]".format(self.package, self.short())
 
 
-version = Version("HtREST", 0, 1, 0)
+version = Version("HtREST", 0, 1, 2)
 """ Version definition of the :mod:`HtREST` module. """
 # version.__name__ = "HtREST"
